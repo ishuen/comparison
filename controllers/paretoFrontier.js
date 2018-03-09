ff3de53795b7236af798f4fb2ada70cfc7708331
ishuen <sandy051122@gmail.com>
2018-03-09 11:28:57 +0800

    add decription on apidoc

--- controllers/paretoFrontier.js
+++ controllers/paretoFrontier.js
@@ -2,6 +2,53 @@ const ParetoFrontier = require('../models/HpbData')
 const pf = require('pareto-frontier')
 const _ = require('lodash')
 class ParetoFrontierController {
+  /**
+  * @api {get} /paretoFrontier/path/:number Request the ranking by Pareto frontier
+  * @apiName ShowPathPareto
+  * @apiGroup Pareto
+  *
+  * @apiParam {Number} number the size of set to rank
+  *
+  * @apiSuccess {Object[]} data ranking result
+  *
+  * @apiSuccessExample {json} Success-Response:
+  * {
+  *   "data": [
+  *     {
+  *       "id": "736",
+  *       "Energy_kcal%": "8.568",
+  *       "Carbohydrate_g%": "0",
+  *       "Protein_g%": "28.36",
+  *       "Totalfat_g%": "13.96875",
+  *       "Saturatedfat_g%": "12.2",
+  *       "Dietaryfibre_g%": "0",
+  *       "Cholestrol_mg%": "23.24",
+  *       "Sodium_mg%": "2.66",
+  *       "RRR'": "1.487370983",
+  *       "T'": "5.87625",
+  *       "T'c": "0.614156564",
+  *       "RRR": "1.215393846",
+  *       "TOri": "0.133812304"
+  *     },
+  *     {
+  *       "id": "711",
+  *       "Energy_kcal%": "33.176",
+  *       "Carbohydrate_g%": "16.97333333",
+  *       "Protein_g%": "40.21333333",
+  *       "Totalfat_g%": "55.828125",
+  *       "Saturatedfat_g%": "42.55",
+  *       "Dietaryfibre_g%": "22.28",
+  *       "Cholestrol_mg%": "20.88",
+  *       "Sodium_mg%": "44.46666667",
+  *       "RRR'": "0.98530405",
+  *       "T'": "39.26190025",
+  *       "T'c": "1.148814965",
+  *       "RRR": "0.885973659",
+  *       "TOri": "0.53480018"
+  *     }
+  *   ]
+  * }
+  */
   showPath (req, res) {
     const number = req.params.number
     const data = ParetoFrontier.randomData(number)
@@ -29,7 +76,7 @@ class ParetoFrontierController {
     let result = left.concat(right.reverse())
     result = [].concat.apply([], result)
     let resData = reorderData(result, data)
-    res.send(resData)
+    res.send({data: resData})
   }
 }
 
