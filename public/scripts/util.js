935c3c9685bc0efc2e11cc4ede671979d13a3b14
ishuen <sandy051122@gmail.com>
2018-07-06 17:49:52 +0800

    sort by scores, reset, init sort

--- public/scripts/util.js
+++ public/scripts/util.js
@@ -1,6 +1,9 @@
 function sortCardsBy(option) {
   var list, i, b, shouldSwitch;
   list = document.getElementById('sourcePanel')
+  block = document.getElementById('sort')
+  buttons = block.getElementsByTagName('button')
+  console.log(buttons)
   let switching = true
   /*Make a loop that will continue until
   no switching has been done:*/
@@ -15,7 +18,7 @@ function sortCardsBy(option) {
       /*check if the next item should
       switch place with the current item:*/
       if (option === 'alphabet') {
-        console.log(i, '***', b[2 * i].getElementsByTagName('h3')[0].innerHTML.toLowerCase())
+        // console.log(i, '***', b[2 * i].getElementsByTagName('h3')[0].innerHTML.toLowerCase())
         let former = b[2 * i].getElementsByTagName('h3')[0].innerHTML.toLowerCase()
         let later = b[2 * (i + 1)].getElementsByTagName('h3')[0].innerHTML.toLowerCase()
         if (former > later) {
@@ -25,6 +28,43 @@ function sortCardsBy(option) {
           shouldSwitch = true
           break
         }
+        for (let j = 1; j < buttons.length; j++) {
+          buttons[j].style.backgroundColor = 'white'
+          buttons[j].style.color = 'black'
+        }
+        buttons[0].style.backgroundColor = 'black'
+        buttons[0].style.color = 'white'
+      } else if (option === 'taste') {
+        let former = parseInt(b[2 * i].getElementsByTagName('td')[0].innerHTML)
+        let later = parseInt(b[2 * (i + 1)].getElementsByTagName('td')[0].innerHTML)
+        if (former > later) {
+          shouldSwitch = true
+          break
+        }
+        for (let j = 0; j < buttons.length; j++) {
+          if (j === 1) {
+            buttons[j].style.backgroundColor = 'black'
+            buttons[j].style.color = 'white'
+          } else {
+            buttons[j].style.backgroundColor = 'white'
+            buttons[j].style.color = 'black'
+          }
+        }
+      } else if (option === 'health') {
+        let former = parseInt(b[2 * i].getElementsByTagName('td')[1].innerHTML)
+        let later = parseInt(b[2 * (i + 1)].getElementsByTagName('td')[1].innerHTML)
+        if (former > later) {
+          shouldSwitch = true
+          break
+        }
+        let last = buttons.length - 1
+        for (let j = last; j >= 0; j--) {
+          buttons[j].style.backgroundColor = 'white'
+          buttons[j].style.color = 'black'
+        }
+        buttons[last].style.backgroundColor = 'black'
+        buttons[last].style.color = 'white'
+
       }
     }
     if (shouldSwitch) {
@@ -34,4 +74,9 @@ function sortCardsBy(option) {
       switching = true
     }
   }
+}
+function init() {
+  document.getElementById('reset').disabled = true;
+ // Move the paragraph from #myDiv1 to #myDiv2
+  $('#sourcePanel').append( $('#destinationPanel>div') );
 }
