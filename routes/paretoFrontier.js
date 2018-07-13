132e1486ca8eb6155571299730403cbc1bef9076
ishuen <sandy051122@gmail.com>
2018-07-13 15:24:35 +0800

    add relaxed pareto, debug

--- routes/paretoFrontier.js
+++ routes/paretoFrontier.js
@@ -2,6 +2,8 @@ var express = require('express')
 var router = express.Router()
 const paretoFrontier = require('../controllers/paretoFrontier')
 
-router.get('/path/:number', paretoFrontier.showPath)
+// router.get('/path/:number', paretoFrontier.showPath)
+router.get('/path/:number', paretoFrontier.relaxedPath)
+router.get('/traditionalPath/:number', paretoFrontier.traditionalPath)
 
 module.exports = router
