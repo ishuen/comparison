5d11684555cb5f7eb8338eb568058453f66ef621
ishuen <sandy051122@gmail.com>
2018-07-22 23:39:51 +0800

    survey4 input check

--- public/scripts/questionSets.js
+++ public/scripts/questionSets.js
@@ -68,7 +68,6 @@ function validateFormSurvey2() {
   let names = $('input').map(function(){ return $(this).attr('name') })
   let distinctNames = $.unique(names)
   distinctNames = distinctNames.filter(function(o) { return (distinctNames[o] !== 'userId' && distinctNames[o] !== 'trial' && distinctNames[o] !== 'itemOrder' && distinctNames[o] !== 'itemId' && distinctNames[o] !== 'taste' && distinctNames[o] !== 'health')})
-  console.log('**', distinctNames)
   for (let qn of distinctNames) {
     let radioSet = document.getElementsByName(qn)
     let selected = false
@@ -80,6 +79,36 @@ function validateFormSurvey2() {
       alert('Please answer all questions.')
       return false
     }
+  }
+  return true
+}
+function validateExpForm() {
+  let form = document.getElementById('form')
+  let ans = form.getElementsByTagName('input')
+  let names = $('input').map(function(){ return $(this).attr('name') })
+  let distinctNames = $.unique(names)
+  distinctNames = distinctNames.filter(function(o) { return (distinctNames[o] !== 'userId' && distinctNames[o] !== 'trial')})
+  for (let qn of distinctNames) {
+    if (qn === 'qn29') {
+      if (document.getElementsByName(qn)[0].value === '') {
+        console.log(qn)
+        alert('Please describe how you sort the items.')
+        return false
+      }
+    } else if (qn === 'qn34') {
+      // no check
+    } else {
+      let radioSet = document.getElementsByName(qn)
+      let selected = false
+      for (let radio of radioSet) {
+        if (radio.checked === true) { selected = true }
+      }
+      if (selected === false) {
+        console.log(qn)
+        alert('Please answer all questions.')
+        return false
+      }
+    } 
   }
   return true
 }
