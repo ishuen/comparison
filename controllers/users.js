2fe7ac9819a077068a1152e2e5abb10e4ca7ca51
ishuen <sandy051122@gmail.com>
2018-07-22 14:08:58 +0800

    add consent form on index page

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,7 @@ const Users = require('../models/Surveys')
 class UsersController {
   getNewUser (req, res, next) {
     Users.getNewId(function (newId) {
-      res.render('index', { title: 'Express', userId: newId })
+      res.render('index', { userId: newId })
     })
   }
 }
