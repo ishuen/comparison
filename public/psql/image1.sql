5f9f7912fa0c013888ef79dcdbd5662e54f62fca
ishuen <sandy051122@gmail.com>
2018-07-30 14:13:14 +0800

    change data set

---
+++ public/psql/image1.sql
@@ -0,0 +1,20 @@
+UPDATE hpbdata SET image = 'https://www.dropbox.com/s/ngr06san3b0gwcr/Caesar%20Salad.jpg?raw=1', taste=4, health=8 WHERE id = '458'; -- Caesar salad
+UPDATE hpbdata SET image = 'https://www.dropbox.com/s/b9iql12hcm0d3ae/Breakfast%20cereal%20mixed%20contains%20whole%20grains.jpg?raw=1', taste=5, health=6 WHERE id = '395'; -- Breakfast cereal mixed contains whole grains
+UPDATE hpbdata SET image = 'https://www.dropbox.com/s/ooblryl7bgqoirg/Bread%20oat%20bran.jpg?raw=1', taste=4, health=6 WHERE id = '372'; -- Bread oat bran
+UPDATE hpbdata SET image = 'https://www.dropbox.com/s/2xy21kcxgen2kgx/Bun%20fruit%20glazed.jpg?raw=1', taste=5, health=5 WHERE id = '426'; -- Bun fruit glazed
+UPDATE hpbdata SET image = 'https://www.dropbox.com/s/ny9g6xmujm40jeg/Pandan%20bread%20enriched.jpg?raw=1', taste=4, health=5 WHERE id = '1885'; -- Pandan bread enriched
+UPDATE hpbdata SET image = 'https://www.dropbox.com/s/cy7c75ngzrw8oge/Bread%20with%20banana%20and%20walnut.jpg?raw=1', taste=4, health=6 WHERE id = '391'; -- Bread with banana and walnut
+UPDATE hpbdata SET image = 'https://www.dropbox.com/s/hkgqcxcrqxujwt2/Bread%20raisin.jpg?raw=1', taste=4, health=5 WHERE id = '374'; -- Bread raisin
+UPDATE hpbdata SET image = 'https://www.dropbox.com/s/sxkjc3lvtzbsuis/Red%20bean%20bun.jpg?raw=1', taste=5, health=6 WHERE id = '2222'; -- Red bean bun
+UPDATE hpbdata SET image = 'https://www.dropbox.com/s/odftw6obh94gxk0/Bread%20garlic.jpg?raw=1', taste=6, health=5 WHERE id = '370'; -- Bread garlic
+UPDATE hpbdata SET image = 'https://www.dropbox.com/s/2rpybdayfi9x24l/Bread%20pita.jpg?raw=1', taste=4, health=5 WHERE id = '373'; -- Bread pita
+UPDATE hpbdata SET image = 'https://www.dropbox.com/s/b54r7ud8r6m9002/Cheese%20and%20ham%20bun.jpg?raw=1', taste=6, health=6 WHERE id = '524'; -- Cheese and ham bun
+UPDATE hpbdata SET image = 'https://www.dropbox.com/s/7kscpy9ya6pe7uo/Bun%20custard.jpg?raw=1', taste=5, health=5 WHERE id = '424'; -- Bun custard
+UPDATE hpbdata SET image = 'https://www.dropbox.com/s/4wsfpykbwgvchge/Chocolate%20cream%20roll.jpg?raw=1', taste=6, health=4 WHERE id = '727'; -- Chocolate cream roll
+UPDATE hpbdata SET image = 'https://www.dropbox.com/s/kiicr6z5osfbnh0/Steamed%20sui%20kow.jpg?raw=1', taste=6, health=6 WHERE id = '2555'; -- Steamed sui kow
+UPDATE hpbdata SET image = 'https://www.dropbox.com/s/rgf12zrp9p4nrg5/Big%20Breakfast%20McDonalds%27.jpg?raw=1', taste=7, health=4 WHERE id = '253'; -- Big Breakfast McDonalds'
+UPDATE hpbdata SET image = 'https://www.dropbox.com/s/so08x12phnjhgyi/Mee%20siam.jpg?raw=1', taste=7, health=5 WHERE id = '1589'; -- Mee siam
+UPDATE hpbdata SET image = 'https://www.dropbox.com/s/hmvoc8i6cltodgx/Sausage%20McMuffin%20with%20egg.jpg?raw=1', taste=7, health=4, foodname='Sausage McMuffin with egg' WHERE id = '2353';
+UPDATE hpbdata SET image = 'https://www.dropbox.com/s/i7q08rrg85giksp/Pancake.jpg?raw=1', taste=6, health=5, foodname='Pancake' WHERE id = '1884';
+UPDATE hpbdata SET image = 'https://www.dropbox.com/s/8lfvwvlph4belzy/Dim%20sum%20siew%20mai%20steamed.jpg?raw=1', taste=7, health=5, foodname='Steamed siew mai' WHERE id = '933';
+UPDATE hpbdata SET image = 'https://www.dropbox.com/s/wi1dkt1xxrd9fau/Muesli%20dried%20fruit%20and%20nuts.jpg?raw=1', taste=5, health=7, foodname='Muesli dried fruit and nuts' WHERE id = '511';
