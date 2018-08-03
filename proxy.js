9585e9798ff047c2d81b236ee060af8ae2056446
ishuen <sandy051122@gmail.com>
2018-08-03 10:02:32 +0800

    change host

---
+++ proxy.js
@@ -0,0 +1,27 @@
+var redbird = require('redbird')
+
+// require() any handler objects here
+// var example = require('./app')
+
+var reverseProxy = redbird(
+  {
+    port: 9000
+  })
+
+reverseProxy.register('http://multisortlist-i.comp.nus.edu.sg', 'http://multisortlist.comp.nus.edu.sg:9080', {useTargetHostHeader: true})
+
+// Attach handlers to the proxy
+// reverseProxy.proxy.on('proxyReq', example.onRequest());
+// reverseProxy.proxy.on('proxyRes', example.onResponse());
+// reverseProxy.proxy.on('proxyReq', example.get());
+// reverseProxy.proxy.on('proxyRes', example.get());
+
+// Optionally, add a handler for errors:
+reverseProxy.proxy.on('error', function (err, req, res) {
+  if (err) throw err
+  res.writeHead(500, {
+    'Content-Type': 'text/plain'
+  })
+
+  res.end('Something went wrong. And we are reporting a custom error message.')
+})
